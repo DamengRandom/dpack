85f1e6b167584267f1e5f6ec10f7513c2cdac1aa
Damon Wu <damonwu0605@gmail.com>
2018-02-25 16:33:21 +1100

    [DP-02] feature/app-initial-deplyment

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'build');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(4323, () => {
+  console.log('Server is up ..');
+});
